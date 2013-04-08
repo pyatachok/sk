ffd95bd3df1f77f7e72ae5183495f1fc9bbf299b
pyatachok <pyatachok@pn1>
2013-04-08 22:02:22 +0300

    создание модели неокончено

---
+++ module/Album/test/AlbumTest/Model/AlbumTest.php
@@ -0,0 +1,62 @@
+<?php
+// module/Album/test/AlbumTest/Model/AlbumTest.php
+
+namespace Album\TestModel;
+
+use Album\Model\Album;
+use PHPUnit_Framework_TestCase;
+
+
+class AlbumTest extends PHPUnit_Framework_TestCase 
+{
+    
+    /**
+     * 1. Все ли свойства альбома изначально равны NULL?
+     */
+    public function testAlbumInitialState()
+    {
+        $album = new Album();
+ 
+        $this->assertNull($album->artist, '"artist" should initially be null');
+        $this->assertNull($album->id, '"id" should initially be null');
+        $this->assertNull($album->title, '"title" should initially be null');
+    }
+    
+    /**
+     * 2. Корректны ли значения свойств в момент вызова метода exchangeArray()?
+     */
+    public function testExchangeArraySetsPropertiesCorrectly()
+    {
+        $album = new Album();
+        $data  = array('artist' => 'some artist',
+                       'id'     => 123,
+                       'title'  => 'some title');
+ 
+        $album->exchangeArray($data);
+ 
+        $this->assertSame($data['artist'], $album->artist, '"artist" was not set correctly');
+        $this->assertSame($data['id'], $album->id, '"id" was not set correctly');
+        $this->assertSame($data['title'], $album->title, '"title" was not set correctly');
+    }
+    
+    /**
+     * 3. Будет ли значение по умолчанию NULL использоваться для свойств, 
+     * ключей которого нет в массиве $data?
+     */
+    public function testExchangeArraySetsPropertiesToNullIfKeysAreNotPresent()
+    {
+        $album = new Album();
+ 
+        $album->exchangeArray(array('artist' => 'some artist',
+                                    'id'     => 123,
+                                    'title'  => 'some title'));
+        $album->exchangeArray(array());
+ 
+        $this->assertNull($album->artist, '"artist" should have defaulted to null');
+        $this->assertNull($album->id, '"id" should have defaulted to null');
+        $this->assertNull($album->title, '"title" should have defaulted to null');
+    }
+    
+    
+    
+}
