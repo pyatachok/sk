582c932a05f92aba437d3e8a8c0de4b4ea3a7974
pyatachok <pyatachok@pn1>
2013-04-17 22:04:11 +0300

    создание роута для модуля AdManager

--- module/AdManager/config/module.config.php
+++ module/AdManager/config/module.config.php
@@ -6,6 +6,25 @@ return array(
             'AdManager\Controller\AdManager' => 'AdManager\Controller\AdManagerController',
         ),
     ),
+    // The following section is new and should be added to your file
+    'router' => array(
+        'routes' => array(
+            'ad-manager' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/ad-manager[/:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'AdManager\Controller\AdManager',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
     'view_manager' => array(
         'template_path_stack' => array(
             'ad-manager' => __DIR__ . '/../view',
