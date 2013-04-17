36ac136c0f7fe8f5de424895b63d45abb0e50679
pyatachok <pyatachok@pn1>
2013-04-17 22:10:07 +0300

    роутинг и контроллер, создание действия и скрипта вида

---
+++ module/AdManager/src/AdManager/Controller/AdManagerController.php
@@ -0,0 +1,17 @@
+<?php
+// module/AdManager/src/AdManager/Controller/AdManagerController.php:
+
+namespace AdManager\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class AdManagerController extends AbstractActionController
+{
+    public function indexAction()
+    {
+	parent::indexAction();
+    }
+}
+
+
